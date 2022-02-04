9a98dc80e7abe5d42de5dd4e03ffa6dd8df4fabd
Nitchamon <Aonoony@gmail.com>
2022-02-04 22:18:04 +0700

    "lab22.h completed"

--- lab22.h
+++ lab22.h
@@ -63,13 +63,38 @@ void Unit::newTurn(){
 	guard_on = false;
 }
 
+bool Unit::isDead(){
+    if(hp<=0) return true;
+    else return false;
+}
 
+void Unit::guard(){
+    guard_on = true;
+}
 
-/////////////////////////////////////////////////////////////////////////////////////
-//Write function members isDead(), guard(), heal(), beAttacked(), and attack() here//
-/////////////////////////////////////////////////////////////////////////////////////
+int Unit::beAttacked(int oppatk){
+    if(guard_on){
+        hp -= (oppatk-def)/3;
+        return (oppatk-def)/3;
+    }else{
+        hp -= oppatk-def;
+        return oppatk-def;
+    }
+}
 
+int Unit::attack(Unit &TypeChar){
+	return TypeChar.beAttacked(atk);
+}
 
+int Unit::heal(){
+    int MyHeal = rand()%21+10;
+    hp += MyHeal;
+    if(hp>hpmax){
+        int re = MyHeal-(hp-hpmax);
+        hp = hpmax;
+        return re;
+    }else return MyHeal;
+}
 
 void drawScene(char p_action,int p,char m_action,int m){
 	cout << "                                                       \n";
